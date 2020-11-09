f59ac37171a02e9b96d83ef312a9e2a19bae1f53
MerjoShine <shinemerjo@gmail.com>
2020-11-09 20:14:54 +0530

    Initial commit

---
+++ student-loan/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
+import './App.css';
+import Dashboard from './components/Navbar/navbar'
+import 'font-awesome/css/font-awesome.min.css';
+
+class App extends React.Component{
+  render(){
+    return(
+      <Router>
+        <Switch>
+          <Route path="/" component={Dashboard} ></Route>
+        </Switch>
+      </Router>
+    )
+  }
+}
+
+export default App;
