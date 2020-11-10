1d98661f619c2583365d579f0c7d386dedffaebc
MerjoShine <shinemerjo@gmail.com>
2020-11-10 14:56:19 +0530

    changes in studentloan

--- student-loan/src/components/StudentLoan/studentLoan.js
+++ student-loan/src/components/StudentLoan/studentLoan.js
@@ -4,56 +4,108 @@ import { Doughnut, Line } from 'react-chartjs-2';
 import { FaChevronCircleRight } from 'react-icons/fa';
 import { FaChevronCircleDown } from 'react-icons/fa';
 
-const state = {
-	labels: [ 'completed', 'Not completed' ],
-	datasets: [
-		{
-			label: 'Rainfall',
-			backgroundColor: [ '#009900', '#C0C0C0' ],
-			hoverBackgroundColor: [ '#009900', '#C0C0C0' ],
-			data: [ 10, 90 ]
-		}
-	]
-};
+// const state = {
+// 	labels: [ 'completed', 'Not completed' ],
+// 	datasets: [
+// 		{
+// 			label: 'Rainfall',
+// 			backgroundColor: [ '#009900', '#C0C0C0' ],
+// 			hoverBackgroundColor: [ '#009900', '#C0C0C0' ],
+// 			data: [ 10, 90 ]
+// 		}
+// 	]
+// };
 
-const state1 = {
-	labels: [ 'Age 25', 'Today', '', 'Age 32' ],
-	datasets: [
-		{
-			fill: true,
-			label: '1',
-			lineTension: 0,
-			backgroundColor: '#009900',
-			data: [ 0, 25 ],
-			pointRadius: 0
-		},
-		{
-			label: '2',
-			fill: true,
-			lineTension: 0,
-			backgroundColor: '#C0C0C0',
-			borderWidth: 0,
-			data: [0,25, 50, 75 ],
-			pointRadius: 0
-		}
-	]
-};
+// const state1 = {
+// 	labels: [ 'Age 25', 'Today', '', 'Age 32' ],
+// 	datasets: [
+// 		{
+// 			fill: true,
+// 			label: '1',
+// 			lineTension: 0,
+// 			backgroundColor: '#009900',
+// 			data: [ 0, 25 ],
+// 			pointRadius: 0
+// 		},
+// 		{
+// 			label: '2',
+// 			fill: true,
+// 			lineTension: 0,
+// 			backgroundColor: '#C0C0C0',
+// 			borderWidth: 0,
+// 			data: [0,25, 50, 75 ],
+// 			pointRadius: 0
+// 		}
+// 	]
+// };
 
 export class Student extends Component {
+	constructor(props){
+		super(props)
+		
+	}
+	
+	empty=this.props.data===0&&
+	this.props.data1===0&&
+	this.props.header===""&&
+	this.props.label1===""&&
+	this.props.label2===""?true:false
+	doughnutData=this.props.data===0?0:this.props.data||10
+
+	lineData=this.props.data1===0?[0]:this.props.data1===25?[0,25]:this.props.data1===50?[0,25,50]:
+	this.props.data1===75?[0,25,50,75]:[0,25]
+	lineDataLabel=this.props.data1===0?[ 'Today', '', '', 'Age 32' ]:this.props.data1===25?[ 'Age 25', 'Today', '', 'Age 32' ]:this.props.data1===50?[ 'Age 25', '', 'Today', 'Age 32' ]:
+	this.props.data1===75?[ 'Age 25', '', '', 'Today' ]:[ 'Age 25', 'Today', '', 'Age 32' ]
+
+	state = {
+		labels: [ 'completed', 'Not completed' ],
+		datasets: [
+			{
+				label: 'Rainfall',
+				backgroundColor: [ this.props.color||'#009900', '#C0C0C0' ],
+				hoverBackgroundColor: [ this.props.color||'#009900', '#C0C0C0' ],
+				data: [ this.doughnutData, 100-this.doughnutData ]
+			}
+		]
+	};
+	
+	state1 = {
+		labels: this.lineDataLabel,
+		datasets: [
+			{
+				fill: true,
+				label: '1',
+				lineTension: 0,
+				backgroundColor: this.props.color|| '#009900',
+				data: this.lineData,
+				pointRadius: 0
+			},
+			{
+				label: '2',
+				fill: true,
+				lineTension: 0,
+				backgroundColor: '#C0C0C0',
+				borderWidth: 0,
+				data: [0,25, 50, 75 ],
+				pointRadius: 0
+			}
+		]
+	};
+	
 	render() {
 		return (
-			<div className="container mt-3">
+			<div className="container mt-3" style={{backgroundColor:this.props.backgroundColor||""}}>
 				<div className="row">
 					<div className="col-md-12 text-left">
 						<h5>Your goals</h5>
-						<h2>Pay off student loan</h2>
+						<h2>{this.props.header===""?<span className="text text-muted">Add header here</span>:this.props.header||"Pay off student loan"}</h2>
 						<div className="row mt-3">
 							<div className="col-md-3 col-sm-12">
-								<h4>Goal progress</h4>
-								<div className="card ca mt-3 bg-light">
-									<div className="txx">10%</div>
+								<h4>{this.props.label1===""?<span className="text text-muted">Add doughnut header</span>:this.props.label1||"Goal progress"}</h4>
+								<div className="card ca mt-3 bg-light" onMouseOver={this.props.onMouseOver}>
+									<div className="txx">{this.doughnutData}%</div>
 									<Doughnut
-										data={state}
+										data={this.state}
 										width={100}
 										height={100}
 										options={{
@@ -84,10 +136,10 @@ export class Student extends Component {
 								</div>
 							</div>
 							<div className="col-md-8 ml-3 col-sm-12">
-								<h4>Goal trajectory</h4>
+								<h4>{this.props.label2===""?<span className="text text-muted">Add line graph header</span>:this.props.label2||"Goal trajectory"}</h4>
 								<div className="card ca1 mt-3 bg-light">
 									<Line
-										data={state1}
+										data={this.state1}
 										height={90}
 										options={{
 											legend: {
@@ -114,7 +166,8 @@ export class Student extends Component {
 									<div className="col-md-3">
 										<button
 											className="btn btn-block text-light"
-											style={{ backgroundColor: '#009900' }}
+											style={this.empty?{ backgroundColor: "#C0C0C0" }:{ backgroundColor: this.props.color||'#009900' }}
+											onClick={this.props.onClick}
 										>
 											Well funded&nbsp;&nbsp;<i class="fa fa-info-circle" aria-hidden="true"></i>
 										</button>
--- student-loan/src/stories/studentLoan.stories.js
+++ student-loan/src/stories/studentLoan.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css'
+import {action} from '@storybook/addon-actions'
 
 import { Student } from '../components/StudentLoan/studentLoan';
 
@@ -8,31 +9,93 @@ export default {
   title: 'Components/StudentLoan',
   component: Student,
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor:{
+      name: 'Background Color',
+      control: {
+        type: 'color'
+      }
+      },
+    label1:{
+      name: 'Doughnut label',
+      type: { name: 'text', required: false },
+      defaultValue: 'Goal progress',
+      control: {
+        type: 'text'
+      }
+      },
+      label2:{
+        name: 'Line label',
+        type: { name: 'text', required: false },
+        defaultValue: 'Goal trajectory',
+        control: {
+          type: 'text'
+        }
+        },
+    header:{
+      name: 'Header',
+      type: { name: 'text', required: false },
+      defaultValue: 'Pay off student loan',
+      control: {
+        type: 'text'
+      }
+      },
+    color:{
+      name: 'Chart color',
+      control: {
+        type: 'color'
+      }
+      },
+    data:{
+        name: 'Doughnut Data',
+        type: { name: 'number', required: false },
+        defaultValue: 10,
+        control: {
+          type: 'number'
+        }
+        },
+    data1:{
+          name: 'Line Data',
+          type: { name: 'number', required: false },
+          defaultValue: 25,
+          control: {
+            type: 'select',
+            options: [
+              0, 
+              25, 
+              50,
+              75
+            ]
+          }
+          }
   },
 };
 
-const Template = (args) => <Student {...args}/>;
+const Template = (args) => <Student onMouseOver={action('Graph hovered')} onClick={action('Clicked')}  {...args}/>;
 
 export const Default = Template.bind({});
-// Default.args = {
-//   primary: true,
-//   label: 'Button',
-// };
+Default.args = {
+  data:10,
+  data1:25,
+  header:"Pay off student loan",
+  label1:"Goal progress",
+  label2:"Goal trajectory"
+};
 
 export const Empty = Template.bind({});
-// Empty.args = {
-//   label: 'Button',
-// };
+Empty.args = {
+  data:0,
+  data1:0,
+  header:"",
+  label1:"",
+  label2:"",
+
+};
 
 export const Normalize = Template.bind({});
-// Normalize.args = {
-//   size: 'large',
-//   label: 'Button',
-// };
-
-// export const Small = Template.bind({});
-// Small.args = {
-//   size: 'small',
-//   label: 'Button',
-// };
+Normalize.args = {
+  data:50,
+  data1:50,
+  header:"Student Loan",
+  label1:"Goal progress",
+  label2:"Goal trajectory"
+};
